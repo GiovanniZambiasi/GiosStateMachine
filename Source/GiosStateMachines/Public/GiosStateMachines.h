8775a4ecda8a272ab751ccbd31eec94680960557
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2024-06-04 20:53:33 -0300

    Fix UE 5.4 error with log macro

--- Source/GiosStateMachines/Public/GiosStateMachines.h
+++ Source/GiosStateMachines/Public/GiosStateMachines.h
@@ -6,7 +6,7 @@
 
 GIOSSTATEMACHINES_API DECLARE_LOG_CATEGORY_EXTERN(LogGiosStateMachines, Display, Display)
 
-#define LOG_GIOS_STATEMACHINES(Verbosity, Format, ...)	UE_LOG(LogGiosStateMachines, Verbosity, Format, __VA_ARGS__)
+#define LOG_GIOS_STATEMACHINES(Verbosity, Format, ...)	UE_LOG(LogGiosStateMachines, Verbosity, Format, ##__VA_ARGS__)
 
 #define GIOS_STATEMACHINES_CATEGORY "Gio's State Machines"
 
